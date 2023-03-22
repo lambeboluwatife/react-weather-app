f6ff7a47abed6724a7ad3ed87bb0e915c8bb7982
lambeboluwatife <boluwatifelambe@gmail.com>
2023-03-22 08:41:09 +0100

    getting users weather data

--- src/App.js
+++ src/App.js
@@ -8,54 +8,36 @@ const App = () => {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
 
+  const [userData, setUserData] = useState(null);
   const [weatherData, setWeatherData] = useState(null);
 
-  // const successCallback = (position) => {
-  //   setLatitude(position.coords.latitude);
-  //   setLongitude(position.coords.longitude);
-  // };
-
-  // const errorCallback = (error) => {
-  //   console.log(error);
-  // };
-
-  // navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showLocation);
-    } else {
-      alert("geolocation is not supported on this browser");
-    }
-  }
-
-  function showLocation(position) {
-    setLatitude(position.coords.latitude);
-    setLongitude(position.coords.longitude);
-  }
-
-  getLocation();
-
-  // const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&APPID=40547464b0a67a8f5d0e5fcd1364a8fe`;
-  const URL =
-    "https://api.openweathermap.org/data/2.5/weather?q=lagos&units=metric&APPID=40547464b0a67a8f5d0e5fcd1364a8fe";
-
   useEffect(() => {
-    fetchWeather();
+    fetch("http://ip-api.com/json")
+      .then((response) => response.json())
+      .then((data) => {
+        setLatitude(data.lat);
+        setLongitude(data.lon);
+        setUserData(data);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
-  const fetchWeather = async () => {
-    const res = await fetch(URL);
-    const data = await res.json();
-
-    setWeatherData(data);
-  };
-
-  console.log(weatherData);
+  useEffect(() => {
+    if (latitude && longitude) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=40547464b0a67a8f5d0e5fcd1364a8fe`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeatherData(data);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [latitude, longitude]);
 
   return (
     <div className="">
-      {weatherData && <Header weatherData={weatherData} />}
+      {weatherData && <Header userData={userData} />}
       {weatherData && <Weather weatherData={weatherData} />}
     </div>
   );
